b73785325f2e8a25c30e580ec2fe47024942aa7f
DevTomUK <tomghdev@gmail.com>
2025-02-05 19:41:17 +0000

    styling buttons

--- src/components/navbar/AccountDropdown.jsx
+++ src/components/navbar/AccountDropdown.jsx
@@ -7,10 +7,10 @@ export default function AccountDropdown({ handleLogOut, isOpen }) {
   const avatarSize = { width: '40px', height: '40px' };
 
   return (
-    <div className={`navbar-account-dropdown ${isOpen ? 'open' : ''}`} onClick={handleLogOut}>
+    <div className={`navbar-account-dropdown ${isOpen ? 'open' : ''}`}>
       {/* Pass sizes as an object */}
       <Avatar size={avatarSize}/>      
-      <button>Log Out</button>
+      <button className='account-logout-button' onClick={handleLogOut}>Log Out</button>
     </div>
   );
 }
